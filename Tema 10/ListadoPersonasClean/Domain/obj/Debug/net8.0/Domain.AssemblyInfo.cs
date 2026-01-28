94b8b85499a6cac30501c5787557354ad095c3d5
angelgarciaguillena <angel.garcia@iesnervion.es>
2026-01-28 22:37:22 +0100

    Cambios

--- Tema 10/ListadoPersonasClean/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Tema 10/ListadoPersonasClean/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17674b0ad40f226b12fb19e83547d622ee3e6b25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+789d1524b38f1e64b14b5f10ffe132344a3f5d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
